7e2da9f0bc2f82d57374b581564862b19d82f076
Chris B <79776618+RiceChrisB@users.noreply.github.com>
2022-01-30 22:10:31 -0500

    Detailed loading message under loading spinner
    - Made a dynamic loading message under the spinner
    - Incorporated an additional message that pops up underneath after a set interval of time

--- src/web/app/components/loading-spinner/loading-spinner.component.ts
+++ src/web/app/components/loading-spinner/loading-spinner.component.ts
@@ -10,6 +10,32 @@ import { Component } from '@angular/core';
 })
 export class LoadingSpinnerComponent {
 
-  constructor() { }
+  // Define the loading_message variables
+  loading_message: string = "";
+  loading_message_2: string = "";
+
+  // Generate an additional message after a certain period of time
+  generateAdditionalMessage() {
+    // Runs for 10 minutes
+    for (let i = 1; i <= 2400; i++) {
+      // Format of setTimeout (message, time [ms])
+      setTimeout(() => { this.loading_message = this.loading_message + "."}, i * 250);
+      // Reset loading data message after 4 dots
+      setTimeout(() => { this.loading_message = "Loading data"}, i * 1000);
+    }
+
+    // Generates the additional loading data message (to calm the user)
+    setTimeout(() => { this.loading_message_2 = "Data is still loading. Please wait" }, 1500);
+
+  }
+
+  // Constructor for the LoadingSpinnerComponent class
+  constructor() {
+    // Define loading_message (needed to make first loop of generateAdditionalMessage() work)
+    this.loading_message = "Loading data.";
+    
+    // Starts tracking when the additional message should pop up
+    this.generateAdditionalMessage();
+  }
 
 }
